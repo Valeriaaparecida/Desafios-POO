8f5f03c60d1d61eba691b9957ebce5bc42b614e0
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-11-07 06:57:50 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroProduto/CadastroProduto/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroProduto
+{
+    internal class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Código: {Codigo}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Preço: {Preco:C2}");
+        }
+
+
+    }
+}
---
+++ CadastroProduto/CadastroProduto/Program.cs
@@ -0,0 +1,14 @@
+﻿using CadastroProduto;
+
+Console.WriteLine("Olá usuário(a)!");
+Console.WriteLine("Bem-vindo ao sistema de cadastro de produtos.");
+Console.WriteLine("Por favor, insira as informações do produto abaixo:");
+Produto produto = new Produto();
+Console.Write("Insira o código do produto: ");
+produto.Codigo = int.Parse(Console.ReadLine()!);
+Console.Write("Insira o nome do produto: ");
+produto.Nome = Console.ReadLine()!;
+Console.Write("Insira o preço do produto: ");
+produto.Preco = decimal.Parse(Console.ReadLine()!);
+
+produto.ExibirInformacoes();
